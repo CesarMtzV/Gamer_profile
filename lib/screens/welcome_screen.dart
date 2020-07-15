8c415cd78acd786b23f12263a3c8cffeaede7b51
CesarMtzV <mc.cesar.art@gmail.com>
2020-07-15 18:08:08 -0500

    Added initial route to welcome screen

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text("Welcome screen"),
+        ],
+      ),
+    );
+  }
+}
