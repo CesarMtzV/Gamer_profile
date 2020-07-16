ced6373acbf668ec4ea8f5503b19c3675504782a
CesarMtzV <mc.cesar.art@gmail.com>
2020-07-16 12:26:19 -0500

    Added basic layout and temporary logo

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -10,7 +10,27 @@ class Welcome extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Text("Welcome screen"),
+          Container(
+            child: Image.asset('assets/images/tempLogo.jpg'),
+          ),
+          Text("Create your profile"),
+          Text("&"),
+          Text("share your tastes"),
+          SizedBox(
+            height: 30,
+          ),
+          FlatButton(
+            child: Text("Sign Up"),
+            onPressed: () {
+              print("Sign up pressed");
+            },
+          ),
+          FlatButton(
+            child: Text("Log in"),
+            onPressed: () {
+              print("Log in pressed");
+            },
+          )
         ],
       ),
     );
