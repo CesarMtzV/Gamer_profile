0fc0063612e65d9596558b36b6fcfe9662f4e2ad
CesarMtzV <mc.cesar.art@gmail.com>
2020-07-16 12:33:08 -0500

    Added styles.dart file with the color palette

---
+++ lib/utilities/styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final kColorGrey = Color(0xFF393E41);
+final kColorLightGrey = Color(0xFFD3D0CB);
+final kColorYellow = Color(0xFFE2C044);
+final kColorLightBlue = Color(0xFF587B7F);
+final kColorBlack = Color(0xFF1E2019);
