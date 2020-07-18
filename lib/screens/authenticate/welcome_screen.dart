b4154ae204703ff6a8252ebba2092b6d4deeda1c
CesarMtzV <mc.cesar.art@gmail.com>
2020-07-18 15:06:21 -0500

    Commented out firebase packages and refactored column widget into a center widget

--- lib/screens/authenticate/welcome_screen.dart
+++ lib/screens/authenticate/welcome_screen.dart
@@ -8,43 +8,44 @@ class Welcome extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            child: Image.asset('assets/images/tempLogo.jpg'),
-            height: 350,
-          ),
-          Text(
-            "Create your profile\n&\nshare your tastes",
-            style: TextStyle(fontSize: 20),
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            width: size.width * 0.8,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: FlatButton(
-                child: Text("Sign Up"),
-                color: kColorYellow,
-                onPressed: () {
-                  print("Sign up pressed");
-                },
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              child: Image.asset('assets/images/tempLogo.jpg'),
+              height: 350,
+            ),
+            Text(
+              "Create your profile\n&\nshare your tastes",
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10),
+              width: size.width * 0.8,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: FlatButton(
+                  child: Text("Sign Up"),
+                  color: kColorYellow,
+                  onPressed: () {
+                    print("Sign up pressed");
+                  },
+                ),
               ),
             ),
-          ),
-          FlatButton(
-            child: Text("Log in"),
-            onPressed: () {
-              print("Log in pressed");
-            },
-          )
-        ],
+            FlatButton(
+              child: Text("Log in"),
+              onPressed: () {
+                print("Log in pressed");
+              },
+            )
+          ],
+        ),
       ),
     );
   }
